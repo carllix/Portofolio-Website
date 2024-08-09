a94d90a2939432b32e8dc8b3b9d7b83d6a2cf57f
carllix <carloangkisan21@gmail.com>
2024-08-09 17:22:39 +0700

    feat: add responsive HomeSection

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -5,32 +5,35 @@ import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 export default function HomeSection() {
   return (
-    <div className="flex w-11/12 mx-auto mt-10 items-center" id="home">
-      <div className="basis-2/3 pr-20">
-        <p className="text-xl">Hi, Everyone!</p>
-        <p className="text-5xl font-extrabold mt-2 mb-5">
+    <div
+      className="flex flex-col-reverse text-center w-11/12 mx-auto mt-10 items-center md:flex-row md:text-left"
+      id="home"
+    >
+      <div className="basis-2/3">
+        <p className="text-md mt-3 md:text-xl md:mt-0">Hi, Everyone!</p>
+        <p className="text-2xl font-extrabold mt-2 md:text-5xl">
           I&#39;m{" "}
           <span className="text-[rgb(135,196,182)]">Carlo Angkisan</span>
         </p>
-        <p className="mb-4 text-lg font-semibold">
+        <p className="text-sm font-semibold mt-2 md:text-lg md:mt-5">
           <span className="text-[rgb(135,196,182)]">Spartan</span> | Frontend
           Developer
         </p>
-        <div className="mb-6 flex space-x-5">
+        <div className="flex space-x-5 mt-3 justify-center md:mt-4 md:justify-start">
           <Link
             href="https://www.linkedin.com/in/carloangkisan"
             target="_blank"
           >
-            <FaLinkedin className="size-6 duration-500 hover:translate-y-[-5px] hover:text-[rgb(135,196,182)]" />
+            <FaLinkedin className="size-5 md:size-6 duration-500 hover:translate-y-[-5px] hover:text-[rgb(135,196,182)]" />
           </Link>
           <Link href="https://github.com/carllix" target="_blank">
-            <FaGithub className="size-6 duration-500 hover:translate-y-[-5px] hover:text-[rgb(135,196,182)]" />
+            <FaGithub className="size-5 md:size-6 duration-500 hover:translate-y-[-5px] hover:text-[rgb(135,196,182)]" />
           </Link>
           <Link href="https://www.instagram.com/carlooangkisan" target="_blank">
-            <FaInstagram className="size-6 duration-500 hover:translate-y-[-5px] hover:text-[rgb(135,196,182)]" />
+            <FaInstagram className="size-5 md:size-6 duration-500 hover:translate-y-[-5px] hover:text-[rgb(135,196,182)]" />
           </Link>
         </div>
-        <div>
+        <div className="mt-5 md:mt-6">
           <Button>
             <Link
               href="https://www.linkedin.com/in/carloangkisan"
@@ -50,7 +53,7 @@ export default function HomeSection() {
           alt="Foto Profil"
           width={500}
           height={500}
-          className="rounded-full"
+          className="rounded-full size-48 md:size-auto"
         />
       </div>
     </div>
