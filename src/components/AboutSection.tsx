0f38d54e7cfb7c9eaf7d61c735bcfcc8b82f8286
carllix <carloangkisan21@gmail.com>
2024-08-09 17:23:48 +0700

    feat: add responsive AboutSection

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
 export default function AboutSection() {
   return (
-    <div className="w-11/12 mx-auto pt-20" id="about">
+    <div className="w-11/12 mx-auto pt-20 px-5 md:px-0" id="about">
       <p className="text-3xl font-black text-center">
         About <span className="text-[rgb(135,196,182)]">Me</span>
       </p>
-      <div className="my-8 flex">
-        <div className="basis-1/3 pr-5">
+      <div className="my-8 flex flex-col md:flex-row">
+        <div className="basis-1/3 md:pr-5">
           <p className="text-center font-semibold text-lg">
             <span className="text-[rgb(135,196,182)]">Who</span> Am I?
           </p>
-          <p className="mt-6 text-justify">
+          <p className="mt-4 md:mt-6 text-justify">
             I&#39;m an Undergraduate Informatics Engineering Student at
             Institute Technology Bandung. I have a strong interest in the world
-            of technology, especially in Software Engineering and Cyber Security. Currently, I am focusing on
-            learning frontend development and plan to study backend development
-            in the future to become a proficient fullstack developer.
+            of technology, especially in Software Engineering and Cyber
+            Security. Currently, I am focusing on learning frontend development
+            and plan to study backend development in the future to become a
+            proficient fullstack developer.
           </p>
         </div>
-        <div className="basis-1/3 border-x px-5">
+        <div className="basis-1/3 mt-8 md:mt-0 md:border-x md:px-5">
           <p className="text-center font-semibold text-lg">
             My <span className="text-[rgb(135,196,182)]">Education</span>
           </p>
@@ -35,7 +36,7 @@ export default function AboutSection() {
             </div>
           </div>
         </div>
-        <div className="basis-1/3 pl-10">
+        <div className="basis-1/3 mt-8 md:mt-0 md:pl-10">
           <p className="text-center font-semibold text-lg">
             My <span className="text-[rgb(135,196,182)]">Hobbies</span>
           </p>
@@ -57,6 +58,20 @@ export default function AboutSection() {
             <Image
               src="/Hobbies/reading.svg"
               alt="reading"
+              width={50}
+              height={50}
+              className="size-10 duration-500 hover:translate-y-[-5px] "
+            />
+            <Image
+              src="/Hobbies/sleep.svg"
+              alt="sleeping"
+              width={50}
+              height={50}
+              className="size-10 duration-500 hover:translate-y-[-5px] "
+            />
+            <Image
+              src="/Hobbies/sleep.svg"
+              alt="sleeping"
               width={50}
               height={50}
               className="size-10 duration-500 hover:translate-y-[-5px] "
